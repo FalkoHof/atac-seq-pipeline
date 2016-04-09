dae11881e4c4ce567b4ec2b95584516ee40f8a19
Falko Hofmann <falkohofmann@gmail.com>
2016-04-09 15:14:51 +0200

    bug fixes and folder structure changes

--- atac_pipe.sh
+++ atac_pipe.sh
@@ -16,8 +16,10 @@ base_dir=/lustre/scratch/users/$USER/atac_seq
 bam_files=$base_dir/bam_files
 bam_files_unmapped=$bam_files/unmapped
 bam_files_aligned=$bam_files/aligned
-#bam_files_uniqe=$bam_files/aligned/unique
-#bam_files_offsetted=$bam_files_uniqe/offsetted
+bam_files_name_sorted=$bam_files_aligned/name_sorted
+bam_files_coordinate_sorted=$bam_files_aligned/coordinate_sorted
+bam_files_uniqe=$bam_files_aligned/unique
+bam_files_offsetted=$bam_files_aligned/offsetted
 
 #folders for bed files
 bed_files=$base_dir/bed_files
@@ -57,6 +59,11 @@ picard=/lustre/scratch/users/$USER/software/picard/picard-tools-2.2.1
 #TODO: complete
 mkdir -p $bam_files_unmapped
 mkdir -p $bam_files_aligned
+mkdir -p $bam_files_name_sorted
+mkdir -p $bam_files_coordinate_sorted
+mkdir -p $bam_files_uniqe
+mkdir -p $bam_files_offsetted
+
 mkdir -p $fastq_files
 mkdir -p $fastqc_output/${NAME}
 
@@ -69,59 +76,77 @@ echo 'Starting ATAC-seq pipeline for '${NAME}
 echo "1 - Start mapping part..."
 #1.1 sort bam file
 echo "1.1 - Name sorting bam file..."
-#samtools sort -n -m 4G -@ 8 -o $bam_files_unmapped/${NAME}.sorted.bam \
-#  $bam_files_unmapped/${NAME}.bam
+samtools sort -n -m 4G -@ 8 -o $bam_files_unmapped/${NAME}.sorted.bam \
+  $bam_files_unmapped/${NAME}.bam
 echo "1.1 - Name sorting bam file... - Done"
 #1.2 run fastqc
 echo "1.2 - Running fastqc..."
-#fastqc -o $fastqc_output/${NAME} $bam_files_unmapped/${NAME}.sorted.bam
+fastqc -o $fastqc_output/${NAME} $bam_files_unmapped/${NAME}.sorted.bam
 echo "1.2 - Running fastqc... - Done"
 #1.3 convert bam to fq
 echo "1.3 - Converting bam to fastq..."
-#bedtools bamtofastq -i $bam_files_unmapped/${NAME}.sorted.bam  \
-#  -fq $fastq_files/${NAME}.end1.fq  \
-#  -fq2 $fastq_files/${NAME}.end2.fq
+bedtools bamtofastq -i $bam_files_unmapped/${NAME}.sorted.bam  \
+  -fq $fastq_files/${NAME}.end1.fq  \
+  -fq2 $fastq_files/${NAME}.end2.fq
 echo "1.3 - Converting bam to fastq... - Done"
 #1.4 align to genome
 echo "1.4 - Starting alignment with bowtie2..."
-#bowtie2 --threads 8 \
-#  --very-sensitive \
-#  --maxins 2000 \
-#  --no-discordant \
-#  --no-mixed \
-#  -x $bt2_index \
-#  -1 $fastq_files/${NAME}.end1.fq \
-#  -2 $fastq_files/${NAME}.end2.fq \
-#  -S $bam_files_aligned/${NAME}.sam \
-#  2> $log_files/${NAME}_bt2_summary.txt
+bowtie2 --threads 8 \
+  --very-sensitive \
+  --maxins 2000 \
+  --no-discordant \
+  --no-mixed \
+  -x $bt2_index \
+  -1 $fastq_files/${NAME}.end1.fq \
+  -2 $fastq_files/${NAME}.end2.fq \
+  -S $temp_dir/${NAME}.sam \
+  2> $log_files/${NAME}_bt2_summary.txt
 echo "1.4 - Starting alignment with bowtie2... - Done"
 #1.5 sort mapped reads and convert to bam
-#samtools view -bS $bam_files_aligned/${NAME}.sam \
-#  | samtools sort -n -m 4G -@ 8 -o $bam_files_aligned/${NAME}.bam -
+samtools view -bS $temp_dir/${NAME}.sam \
+  | samtools sort -I 9 -n -m 4G -@ 8 -o $bam_files_name_sorted/${NAME}.bam -
 
-#rm $bam_files_aligned/${NAME}.sam
+samtools view -bS $temp_dir/${NAME}.sam \
+  | samtools sort -I 9 -m 4G -@ 8 -o $bam_files_coordinate_sorted/${NAME}.bam
+
+rm $temp_dir/${NAME}.sam
 echo "1 - Finished mapping part."
 ##2.file conversions
 echo "2 - Starting post processing..."
 #2.1 remove duplicates
 echo "2.1 - Removing duplicates..."
 #java -Djava.io.tmpdir=$temp_dir -jar $picard/picard.jar \
-java -jar -Xmx60g $picard/picard.jar MarkDuplicates I=$bam_files_aligned/${NAME}.bam \
-  O=$bam_files_aligned/${NAME}_unique.bam M=$log_files/${NAME}_dup_metrics.txt \
+java -jar -Xmx60g $picard/picard.jar MarkDuplicates \
+  I=$bam_files_coordinate_sorted/${NAME}.bam \
+  O=$bam_files_uniqe/${NAME}_unique.bam M=$log_files/${NAME}_dup_metrics.txt \
   AS=true REMOVE_DUPLICATES=true TMP_DIR=$TMPDIR
 echo "2.1 - Removing duplicates... - Done"
 #2.2 convert to bed file
 echo "2.2 - Converting bam to bed..."
-bedtools bamtobed -i $bam_files_aligned/${NAME}_unique.bam > $bed_files/${NAME}_unique.bed
+bedtools bamtobed -i $bam_files_uniqe/${NAME}_unique.bam > $bed_files/${NAME}_unique.bed
+
 echo "2.2 - Converting bam to bed... - Done"
 #2.3 sort bed file by mate id
-#sort -k4,4 -t $'\t' $WSEQ/${NAME}/bowtie/accepted_hits_bt.bed > $WSEQ/${NAME}/bowtie/accepted_hits_mate_sorted_bt.bed
+sort -k4,4 -t $'\t' $bed_files/${NAME}_unique.bed > $temp_dir/${NAME}_unique_mate_sorted.bed
+sort -k4,4 -t $'\t' $bed_files/${NAME}.bed > $temp_dir/${NAME}_mate_sorted.bed
+
 #2.4 offset data
 echo "2.4 - Offsetting data..."
-python $pipe_dir/add_offset_for_fp.py $bed_files/${NAME}_unique.bed $bed_files/${NAME}_unique_offset.bed
+python $pipe_dir/add_offset_for_fp.py $temp_dir/${NAME}_unique_mate_sorted.bed \
+  $bed_files/${NAME}_unique_offset.bed
+python $pipe_dir/add_offset_for_fp.py $temp_dir/${NAME}_mate_sorted.bed \
+  $bed_files/${NAME}_offset.bed
+
+rm $temp_dir/${NAME}_mate_sorted.bed
+rm $temp_dir/${NAME}_unique_mate_sorted.bed
+
 #2.5 convert back to bam file
 bedToBam -i $bed_files/${NAME}_unique_offset.bed -g $tair10_genome_size \
-  > $bam_files_aligned/${NAME}_unique_offset.bam
+  | samtools sort -I 9 -m 4G -@ 8 -o $bam_files_offsetted/${NAME}_unique_offset.bam
+
+bedToBam -i $bed_files/${NAME}_offset.bed -g $tair10_genome_size \
+  | samtools sort -I 9 -m 4G -@ 8 -o $bam_files_offsetted/${NAME}_offset.bam
+
 echo "2.4 - Offsetting data... - Done"
 
 #echo "2.5 - Extracting read length..."
