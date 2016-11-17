4539f68003333f61e1486dda1f27594a3deb38d6
Falko Hofmann <falko.hofmann@gmi.oeaw.ac.at>
2016-11-17 17:58:32 +0100

    rewrote peakcalling pipe

--- peak_calling_pipe.sh
+++ peak_calling_pipe.sh
@@ -5,143 +5,167 @@
 #PBS -j oe
 #PBS -q workq
 #PBS -o /lustre/scratch/users/falko.hofmann/log/160526_atac-seq/160526_atac-seq_^array_index^_peak_calling.log
-#PBS -l walltime=6:00:00
+#PBS -l walltime=24:00:00
 #PBS -l select=1:ncpus=8:mem=34gb
 
 # === begin ENVIRONMENT SETUP ===
-####set to 0 (false) or 1 (true) to let the repsective code block run
-#1.split and filter bam files
-filter_bam=0
-#2. peak calling via fseq
-run_fseq=1
-#3. peak calling via macs2
-run_macs2=0
+pipe_dir=/lustre/scratch/users/$USER/pipelines/atac-seq-pipeline
+#set ouput base dir
+base_dir=/lustre/scratch/users/$USER/atac-seq
+#TODO: specify paths here...
+threads=8
+#download and compile fseq from git & set max heap space in fseq exectuable
+fseq=/lustre/scratch/users/$USER/software/F-seq/dist~/fseq/bin/fseq
+#location of the mapping file for the array job
+pbs_mapping_file=$pipe_dir/pbs_mapping_file.txt
+#super folder of the temp dir, script will create subfolders with $sample_name
+temp_dir_base=$base_dir/temp
+#json filters for bamtools
+subnucl_filter=$pipe_dir/bamtools_filter/bamtools_subnucl.json
+nucl_filter=$pipe_dir/bamtools_filter/bamtools_polynucl.json
 
-#pbs array for different samples
-case ${PBS_ARRAY_INDEX} in
-        1) NAME='bc_50_proto_160416';;
-        2) NAME='bc_50_proto_160516';;
-        3) NAME='mg_50_proto_160516';;
-        4) NAME='tp_100_proto_160516';;
-        5) NAME='test_1';;
-        6) NAME='test_2';;
-esac
-
-##### Obtain Parameters from mapping file using $PBS_ARRAY_INDEX as the line number #####
-#mapping_file=$base_dir/pbs_mapping_file.txt
-#input_mapper=`sed -n "${PBS_ARRAY_INDEX} p" $mapping_file`
-#names_mapped=($input_mapper)
-#NAME=${names_mapped[1]}
-
-#set base dirs
-base_dir=/lustre/scratch/users/$USER/atac_seq
-pipe_dir=/lustre/scratch/users/$USER/pipes/atac-seq-pipeline
-
-bam_files=$base_dir/bam_files/aligned/name_sorted
-bam_files_coordinate_sorted=$base_dir/bam_files/aligned/coordinate_sorted
-
-peak_dir=$base_dir/peak_calling
-output_macs2=$peak_dir/macs2
-output_fseq=$peak_dir/fseq
-
-split_bam_files=$bam_files/split_bam
-split_bam_coordiante=$bam_files_coordinate_sorted/split_bam
-
-split_bed_files=$base_dir/bed_files/split_bed
-
-#set background control path and effective genome length for macs2
-effective_genome_size=1.2e8
-#control=$bam_files/50k_bc_background_160326.bam
-
+#TODO: clean up modules
 # Load the required modules
 module load SAMtools/1.3-goolf-1.4.10
 module load BamTools/2.4.0-goolf-1.4.10
-module load MACS/2.1.0.20150420.1-goolf-1.4.10-Python-2.7.5
 module load BEDTools/v2.17.0-goolf-1.4.10
 module load Java/1.8.0_66
-#download and compile fseq from git & set max heap space in fseq binary
-fseq=/lustre/scratch/users/$USER/software/F-seq/dist~/fseq/bin/fseq
+
+##### Obtain Parameters from mapping file using $PBS_ARRAY_INDEX as line number
+input_mapper=`sed -n "${PBS_ARRAY_INDEX} p" $pbs_mapping_file` #read mapping file
+names_mapped=($input_mapper)
+sample_dir=${names_mapped[1]} # get the sample dir
+sample_name=`basename $sample_dir` #get the base name of the dir as sample name
+
+temp_dir=$temp_dir_base/sample_name
+
+mkdir -p $temp_dir
+
+#print some output for logging
+echo '#########################################################################'
+echo 'Starting ATAC-seq peak-calling pipeline for: ' $sample_name
+echo 'Sample directory: ' $sample_dir
+echo 'Mapping file: ' $pbs_mapping_file
+#IDEA: maybe add some diagnostic output the chosen modes.
+echo '#########################################################################'
+
+#some error handling function
+function error_exit
+{
+  echo "$1" 1>&2
+  exit 1
+}
+
+#TODO and conditional initilization..
+aligner_dirs=()
+split_dirs=()
+bed_dirs=()
+peaks_dirs=()
+
+if [ $bowtie_1 -eq 1 ]; then
+  bt_1_files=$sample_dir/bowtie
+  bt_1_split=$bt_1_files/split_bam
+  bt_1_bed=$bt_1_files/bed_files
+  bt_1_peaks=$bt_1_files/peak_calling
+
+  mkdir -p bt_1_split
+  mkdir -p bt_1_peaks
+  mkdir -p bt_1_bed
+
+  aligner_dirs+=(bt_1_files)
+  split_dirs+=(bt_1_split)
+  bed_dirs+=(bt_1_bed)
+  peaks_dirs+=(bt_1_peaks)
+fi
+
+if [ $bowtie_2 -eq 1 ]; then
+  bt_2_files=$sample_dir/bowtie2
+  bt_2_split=$bt_2_files_split/split_bam
+  bt_2_bed=$bt_1_files/bed_files
+  bt_2_peaks=$bt_2_files/peak_calling
+
+  mkdir -p bt_2_split
+  mkdir -p bt_2_peaks
+  mkdir -p bt_2_bed
+
+  aligner_dirs+=(bt_2_files)
+  split_dirs+=(bt_2_split)
+  bed_dirs+=(bt_2_bed)
+  peaks_dirs+=(bt_2_peaks)
+fi
 # === end ENVIRONMENT SETUP ===
+##### Starting the ATAC-seq peak calling pipeline #####
+for (( i = 0 ; i < ${#aligner_dirs[@]} ; i++ )); do
 
-##### Startint the ATAC-seq peak calling pipeline #####
-echo 'Starting peak calling ATAC-seq pipeline for '${NAME}
-#1.split and filter bam files
-if [ $filter_bam -eq 1 ]; then
-  echo "1 - Preparing bam files..."
-  #make output folders
-  mkdir -p $split_bam_files
-  mkdir -p $split_bam_coordiante
-  mkdir -p $bam_files_coordinate_sorted/split_bam
-  #1.1 split bam files into by size ( <100, and nucleosome sizes)
-  echo "1.1 - Splitting bam files to nucleosomal & nucleosomal..."
-  bamtools filter -in $bam_files/${NAME}.bam \
-    -out $split_bam_files/${NAME}.subnucl.bam \
-    -script $pipe_dir/bamtools_filter/bamtools_subnucl.json
+  f=($(ls ${aligner_dirs[$i]} | grep -e ".sorted.bam"))
+  
+  if [[ "${#f[@]}" -ne "1" ]]; then
+    error_exit "Error: wrong number of bam files in folder. Files present: ${#f[@]}"
+  fi
 
-  bamtools filter -in $bam_files/${NAME}.bam \
-    -out $split_bam_files/${NAME}.polynucl.bam \
-    -script $pipe_dir/bamtools_filter/bamtools_polynucl.json
-  echo "1.1 - Splitting bam files to nucleosomal & nucleosomal... - Done"
-  #1.2 sort the bam files
-  echo "1.2 - Sorting bam files..."
-  samtools sort -m 4G -@ 8 -o $split_bam_coordiante/${NAME}.subnucl.bam \
-    $split_bam_files/${NAME}.subnucl.bam
+  #FIXME: switch to a loop based system..
+  echo "Splitting bam files..."
+  #get the subnucleosomal reads and sort them
+  bamtools filter -in ${aligner_dirs[$i]}/$f -script $subnucl_filter | \
+    samtools sort -m 3G -@ $threads - -o ${split_dirs[$i]}/${f%.*}.subnucl.bam
+  samtools index ${split_dirs[$i]}/${f%.*}.subnucl.bam
+  #get the nucleosomal reads and sort them
+  bamtools filter -in ${aligner_dirs[$i]}/$f -script $nucl_filter | \
+    samtools sort -m 3G -@ $threads - -o ${split_dirs[$i]}/${f%.*}.nucl.bam
+  samtools index ${split_dirs[$i]}/${f%.*}.nucl.bam
+  #calculate some stats...
+  reads_subnucl=$(samtools view -c -f 1 ${split_dirs[$i]}/${f%.*}.subnucl.bam)
+  reads_nucl=$(samtools view -c -f 1 ${split_dirs[$i]}/${f%.*}.nucl.bam)
+  enrichment_subncl = $((reads_subnucl / reads_nucl))
+  #some string assignments
+  read_stats="Subnucleosomal reads: $reads_subnucl\n"
+  read_stats+="Nucleosomal reads: $reads_nucl\n"
+  read_stats+="Ratio: $enrichment_subncl"
+  #print and save them
+  printf $read_stats | tee ${split_dirs[$i]}/ratios.txt
+  echo "Splitting bam files... - Done"
 
-  samtools sort -m 4G -@ 8 -o $split_bam_coordiante/${NAME}.polynucl.bam \
-    $split_bam_files/${NAME}.polynucl.bam
-  echo "1.2 - Sorting bam files... - Done"
-fi
+  echo "Converting bam files to bed..."
+  #convert to bed, keep reads from nuclear chromosomes, then sort and store them
+  bedtools bamtobed -i ${aligner_dirs[$i]}/$f | grep "^Ath_chr[1-5]" |\
+    sort -k1,1V -k2,2n > ${bed_dirs[$i]}/${f%.*}.bed
+  bedtools bamtobed -i ${split_dirs[$i]}/${f%.*}.subnucl.bam | \
+    grep "^Ath_chr[1-5]" | sort -k1,1V -k2,2n > ${bed_dirs[$i]}/${f%.*}.subnucl.bed
+  bedtools bamtobed -i ${split_dirs[$i]}/${f%.*}.nucl.bam | \
+    grep "^Ath_chr[1-5]" | sort -k1,1V -k2,2n > ${bed_dirs[$i]}/${f%.*}.nucl.bed
+  echo "Converting bam files to bed... - Done"
+  #make some output folders
+  mkdir -p ${peaks_dirs[$i]}/combined
+  mkdir -p ${peaks_dirs[$i]}/subnucl
+  mkdir -p ${peaks_dirs[$i]}/nucl
 
-#2. peak calling via fseq
-##  use fseq to call peaks for the subnucleosomal reads, as it is sensitiver
-##  than macs2
-if [ $run_fseq -eq 1 ]; then
-  echo "2 - Starting fseq peak calling..."
-  #make ouput folders
-  mkdir -p $split_bed_files
-  mkdir -p $output_fseq/${NAME}
-  #2.1 convert subnucleosomal bam to bed file for fseq
-  echo "2.1 - Converting bam to bed..."
-  bedtools bamtobed -i $split_bam_coordiante/${NAME}.subnucl.bam \
-    > $split_bed_files/${NAME}.subnucl.bed
-  echo "2.1 - Converting bam to bed... - Done"
-  #2.2 call peaks with fseq
-  echo "2.2 - Calling peaks with fseq"
-  sh $fseq -v -f 0 -of npf -t 1.0 -o $output_fseq/${NAME} \
-    $split_bed_files/${NAME}.subnucl.bed
-  echo "2.2 - Calling peaks with fseq... - Done"
-  echo "2.3 - Cleaning up..."
-  #concatenate nuclear chromosomes to one file
-  cd $output_fseq/${NAME}
-  cat `ls | grep -e "Ath_chr[1-5].npf"` | sort -k 1,1 -k2,2n \
-    > $output_fseq/${NAME}_fseq.np
-  cd
-  #clean up and remove unecessary files
-  rm -vr $output_fseq/${NAME}
-  echo "2.3 - Cleaning up... - Done"
-fi
+  echo "Peak-calling with f-seq..."
+  #run fseq on the different files...
+  sh $fseq -v -f 0 -of npf -t 1.0 -o ${peaks_dirs[$i]}/combined \
+    ${bed_dirs[$i]}/${f%.*}.bed
+  sh $fseq -v -f 0 -of npf -t 1.0 -o ${peaks_dirs[$i]}/subnucl \
+    ${bed_dirs[$i]}/${f%.*}.subnucl.bed
+  sh $fseq -v -f 0 -of npf -t 1.0 -o ${peaks_dirs[$i]}/nucl \
+    ${bed_dirs[$i]}/${f%.*}.nucl.bed
+  echo "Peak-calling with f-seq... - Done"
 
-#3. peak calling via macs2
-##  call narrow and broad peaks with macs2. The strength of macs2 is however
-##  to call the broad peak regions of the polynucleosomal tracks
-if [ $run_macs2 -eq 1 ]; then
-  echo "3 - Starting MACS2 peak calling..."
-  # make folders and set temp dir to prevent memory issues
-  export TMPDIR=$WORKDIR/macs_tmp
-  mkdir -p $TMPDIR
-  mkdir -p $output_macs2/narrow
-  mkdir -p $output_macs2/broad
-  #2.1 call narrow peaks with macs2 in the subnucleosomal fraction
-  echo "2.1 - Calling narrow peaks..."
-  macs2 callpeak -t $split_bam_files/${NAME}.subnucl.bam -f BAMPE \
-    -g $effective_genome_size -n ${NAME}_subnucl --outdir $output_macs2/narrow \
-    --nomodel --shift -50 --extsize 100 -B -q 0.01
-  echo "2.1 - Calling narrow peaks... - Done"
-  #2.2 call broad peaks with macs2 in the polynucleosomal fraction
-  echo "2.2 - Calling broad peaks..."
-  macs2 callpeak --broad -t $split_bam_files/${NAME}.polynucl.bam -c $control -f BAMPE \
-    -g $effective_genome_size -n ${NAME}_background --outdir $output_macs2/broad \
-    --nomodel --shift -37 --extsize 73 -B -q 0.01
-  echo "2.2 - Calling broad peaks... - Done"
-fi
-echo "Peak calling pipeline for ${NAME} complete."
+  echo "Merging f-seq files..."
+  #put the fseq output file names in some arrays
+  fseq_combined=($(ls ${peaks_dirs[$i]}/combined | grep -e "Ath_chr[1-5].npf"))
+  fseq_subnucl=($(ls ${peaks_dirs[$i]}/subnucl | grep -e "Ath_chr[1-5].npf"))
+  fseq_nucl=($(ls ${peaks_dirs[$i]}/nucl | grep -e "Ath_chr[1-5].npf"))
+  #concatenate them to one file and sort
+  cat ${fseq_combined[@]} | sort -k 1,1 -k2,2n > \
+    ${peaks_dirs[$i]}/combined/${f%.*}_fseq.np
+  cat ${fseq_subnucl[@]} | sort -k 1,1 -k2,2n > \
+    ${peaks_dirs[$i]}/subnucl/${f%.*}_fseq.np
+  cat ${fseq_nucl[@]} | sort -k 1,1 -k2,2n > \
+    ${peaks_dirs[$i]}/nucl/${f%.*}_fseq.np
+  echo "Merging f-seq files... - Done"
+
+  #TODO: implement removal of the old per chromosome files.
+  #rm -vr ${fseq_combined[@]}
+  #rm -vr ${fseq_subnucl[@]}
+  #rm -vr ${fseq_nucl[@]}
+done
+#TODO: implement clean loop that removes or zips the bed files.
