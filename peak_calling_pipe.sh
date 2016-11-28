6c352a6a6a819607b4e0791fed5ecbfa91d9ce6f
Falko Hofmann <falko.hofmann@gmi.oeaw.ac.at>
2016-11-28 16:30:20 +0100

    accidently removed loading of samtools model. restored it.

--- peak_calling_pipe.sh
+++ peak_calling_pipe.sh
@@ -58,6 +58,7 @@ echo '#########################################################################'
 ml BamTools/2.4.0-foss-2016a
 ml BEDTools/2.26.0-foss-2016a
 ml Java/1.8.0_66
+ml SAMtools/1.3.1-foss-2016a
 
 
 #some error handling function
